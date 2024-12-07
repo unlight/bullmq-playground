3bee69694e8c70a7e7adebe096bdb115fde480a6
Roman <s91630277@gmail.com>
2024-12-07 19:18:25 +0400

    2

--- src/example2.ts
+++ src/example2.ts
@@ -4,48 +4,48 @@ import { setTimeout } from 'node:timers/promises';
 const queueName = 'cars';
 const queue = new Queue(queueName);
 
-const worker = new Worker(
-  queueName,
-  async (job: Job) => {
-    await job.updateProgress(1);
-    console.log('started process for job', job.id, job.name);
-    await setTimeout(2000);
-    console.log('ended process for job', job.id, job.name);
-  },
-  {
-    autorun: false,
-    connection: { url: 'redis://localhost:6379' },
-    concurrency: 2,
-  },
-);
+await queue.obliterate({ force: true });
 
-worker.on('ready', () => {
-  console.log('worker ready');
-});
-worker.on('active', job => {
-  console.log('job active', job.id, job.name);
-});
-worker.on('completed', job => {
-  console.log(`job with id ${job.id} ${job.name} has been completed`);
-});
-worker.on('drained', () => {
-  console.log('worker drained'); // опустошён
+await queue.add('car 1', 'job data A');
+await queue.add('car 1', 'job data B');
+await queue.add('car 2', 'job data C');
+await queue.add('car 3', 'job data D');
+
+queue.on('waiting', (job: Job) => {
+  console.log(`waiting job ${job.id} ${job.name}`);
 });
 
-queue.on('cleaned', (...args) => console.log('cleaned', ...args));
-queue.on('error', (...args) => console.log('error', ...args));
-queue.on('paused', (...args) => console.log('paused', ...args));
-queue.on('progress', (...args) => console.log('progress', ...args));
-queue.on('removed', (...args) => console.log('removed', ...args));
-queue.on('resumed', (...args) => console.log('resumed', ...args));
-queue.on('waiting', job => console.log('queue waiting job id', job.id));
+createWorker(1);
+createWorker(2);
 
-await queue.add('shipment 1', { name: 'shipment 1' });
-await queue.add('shipment 1', { name: 'shipment 1' });
-await queue.addBulk([
-  { name: 'shipment 1', data: { name: 'shipment 1' } },
-  { name: 'shipment 2', data: { name: 'shipment 2' } },
-  { name: 'shipment 3', data: { name: 'shipment 3' } },
-]);
+function createWorker(n: number) {
+  const worker = new Worker(
+    queueName,
+    async (job: Job) => {
+      console.log(
+        `< started process for job ${job.id} ${job.data} in worker ${n}`,
+      );
+      await setTimeout(2000); // Emulate long processing
+      console.log(
+        `> ended process for job ${job.id} ${job.data} in worker ${n}`,
+      );
+    },
+    {
+      connection: { url: 'redis://localhost:6379' },
+      concurrency: 1,
+    },
+  );
 
-await worker.run();
+  worker.on('ready', () => {
+    console.log(`worker ${n} ready`);
+  });
+  worker.on('active', job => {
+    console.log(`job ${job.id} ${job.data}  active in worker ${n}`);
+  });
+  worker.on('completed', job => {
+    console.log(`job with id ${job.id} ${job.data} has been completed`);
+  });
+  worker.on('drained', () => {
+    console.log(`worker ${n} drained`);
+  });
+}
